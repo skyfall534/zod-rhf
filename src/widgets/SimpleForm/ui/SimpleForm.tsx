280882ea281a9090b7c97e35d620afb32f5ddda6
Михайлов Алексей Геннадьевич <Mikhaylov.AG@gazprom-neft.local>
2026-02-19 15:57:20 +0300

    feat: add more fields and verifications

--- src/widgets/SimpleForm/ui/SimpleForm.tsx
+++ src/widgets/SimpleForm/ui/SimpleForm.tsx
@@ -1,45 +1,97 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { isValidPhoneNumber } from "libphonenumber-js";
 import { parsePhoneNumber } from "libphonenumber-js/min";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
+import { DatePicker } from "@consta/uikit/DatePicker";
+
 import z from "zod";
 
-const formSchema = z.object({
-  name: z
-    .string("Неверный формат")
-    .nonempty("Введите имя")
-    .min(2, "Ваше имя слишком короткое")
-    .max(20, "Ваше имя слишком длинное"),
-  surname: z
-    .string("Неверный формат")
-    .nonempty("Введите фамилию")
-    .min(2, "Ваша фамилия слишком короткая")
-    .max(20, "Ваша фамилия слишком длинная"),
-  email: z.email("Введите корректный email"),
-  phone: z
-    .string()
-    .nonempty("Введите номер телефона")
-    .refine(isValidPhoneNumber, "Введите корректный номер телефона")
-    .transform((value) => parsePhoneNumber(value).number.toString()),
-  age: z
-    .number("Введите число")
-    .min(18, "Введите возраст от 18 до 65")
-    .max(65, "Введите возраст от 18 до 65"),
-});
+const reservedEmails = [
+  "test123@gmail.com",
+  "test345@gmail.com",
+  "test456@gmail.com",
+];
+
+const formSchema = z
+  .object({
+    name: z
+      .string("Неверный формат")
+      .nonempty("Введите имя")
+      .min(2, "Слишком мало символов")
+      .max(32, "Превышено максимально кол-во символов")
+      .nonempty("Поле обязательно к заполнению"),
+    surname: z
+      .string("Неверный формат")
+      .nonempty("Введите фамилию")
+      .min(2, "Слишком мало символов")
+      .max(32, "Превышено максимально кол-во символов")
+      .nonempty("Поле обязательно к заполнению"),
+    email: z
+      .email("Введите корректный email")
+      .refine((value) => {
+        return !reservedEmails.includes(value);
+      }, "Email уже используется")
+      .nonempty("Поле обязательно к заполнению"),
+    password: z
+      .string()
+      .nonempty("Поле обязательно к заполнению")
+      .min(8, "Пароль должен содеражть минимум 8 символов")
+      .refine(
+        (val) => /[A-Z]/.test(val),
+        "Пароль должен включать минимум одну заглавную букву"
+      )
+      .refine(
+        (val) => /[^A-Za-zd]/.test(val),
+        "Пароль должен включать хотя бы один спец символ"
+      )
+      .refine(
+        (val) => /[0-9]/.test(val),
+        "Пароль должен включать хотя бы одну цифру"
+      ),
+    confirmPassword: z
+      .string()
+      .nonempty("Поле обязательно к заполнению")
+      .min(8, "Пароль должен содеражть минимум 8 символов")
+      .refine(
+        (val) => /[A-Z]/.test(val),
+        "Пароль должен включать минимум одну заглавную букву"
+      )
+      .refine(
+        (val) => /[^A-Za-zd]/.test(val),
+        "Пароль должен включать хотя бы один спец символ"
+      )
+      .refine(
+        (val) => /[0-9]/.test(val),
+        "Пароль должен включать хотя бы одну цифру"
+      ),
+    phone: z
+      .string()
+      .nonempty("Введите номер телефона")
+      .refine(isValidPhoneNumber, "Введите корректный номер телефона")
+      .transform((value) => parsePhoneNumber(value).number.toString()),
+    age: z
+      .number("Введите число")
+      .min(18, "Введите возраст от 18 до 65")
+      .max(65, "Введите возраст от 18 до 65"),
+    gender: z.enum(["W", "M"]),
+    newsletter: z.boolean().optional().default(false),
+    birthdate: z.date(),
+  })
+  .refine(
+    (data) => data.password === data.confirmPassword,
+    "Пароли не совпадают"
+  );
 
 export const SimpleForm = () => {
   const {
     watch,
     register,
     handleSubmit,
-
+    control,
     formState: { errors },
   } = useForm({ resolver: zodResolver(formSchema) });
 
   const onSubmit = (data) => console.log(data);
-
-  console.log(errors);
 
   return (
     <div style={{ width: "23%" }}>
@@ -49,12 +101,7 @@ export const SimpleForm = () => {
       >
         <div style={{ display: "flex", flexDirection: "column" }}>
           <span style={{ textAlign: "left" }}>Имя</span>
-          <input
-            type="text"
-            placeholder="Введите имя"
-            {...register("name")}
-            aria-invalid={true}
-          />
+          <input type="text" placeholder="Введите имя" {...register("name")} />
           {errors.name && (
             <span style={{ textAlign: "left", fontSize: "12px" }}>
               {errors.name.message}
@@ -101,6 +148,86 @@ export const SimpleForm = () => {
           )}
         </div>
         <div style={{ display: "flex", flexDirection: "column" }}>
+          <span style={{ textAlign: "left" }}>Пароль</span>
+          <input
+            type="password"
+            placeholder="Введите пароль"
+            {...register("password")}
+          />
+          {errors.password && (
+            <span style={{ textAlign: "left", fontSize: "12px" }}>
+              {errors.password.message}
+            </span>
+          )}
+          {errors.root && (
+            <span style={{ textAlign: "left", fontSize: "12px" }}>
+              {errors.root.message}
+            </span>
+          )}
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <span style={{ textAlign: "left" }}>Подтвержение пароля</span>
+          <input
+            type="password"
+            placeholder="Введите пароль"
+            {...register("confirmPassword")}
+          />
+          {errors.confirmPassword && (
+            <span style={{ textAlign: "left", fontSize: "12px" }}>
+              {errors.confirmPassword.message}
+            </span>
+          )}
+          {errors.root && (
+            <span style={{ textAlign: "left", fontSize: "12px" }}>
+              {errors.root.message}
+            </span>
+          )}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+          }}
+        >
+          <span style={{ textAlign: "left" }}>Пол</span>
+          <div>
+            <input
+              type="radio"
+              id="genderM"
+              value="M"
+              {...register("gender")}
+            />
+            <label htmlFor="genderM">M</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="genderW"
+              value="W"
+              {...register("gender")}
+            />
+            <label htmlFor="genderW">Ж</label>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+          }}
+        >
+          <span style={{ textAlign: "left" }}>Подписка на новости</span>
+          <div>
+            <input
+              type="checkbox"
+              id="newsletter"
+              {...register("newsletter")}
+            />
+            <label htmlFor="newsletter">Подписка</label>
+          </div>
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
           <span style={{ textAlign: "left" }}>Мобильный телефон</span>
           <input
             type="tel"
@@ -110,6 +237,19 @@ export const SimpleForm = () => {
           {errors.phone && (
             <span style={{ textAlign: "left", fontSize: "12px" }}>
               {errors.phone.message}
+            </span>
+          )}
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <span style={{ textAlign: "left" }}>Дата рождения</span>
+          <Controller
+            name="birthdate"
+            render={({ field }) => <DatePicker {...field} />}
+            control={control}
+          />
+          {errors.birthdate && (
+            <span style={{ textAlign: "left", fontSize: "12px" }}>
+              {errors.birthdate.message}
             </span>
           )}
         </div>
